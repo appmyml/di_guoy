11e35732bd30b0d693f0ae2d97e56e8051c9690d
kyle <aw123e@126.com>
2022-02-22 13:43:40 +0800

    .

--- dd_cookie.js
+++ dd_cookie.js
@@ -5,17 +5,4 @@ let tokens = [
 
 ];
 
-// 环境变量的token
-if (process.env.DD_TOKEN) {
-  if (process.env.DD_TOKEN.indexOf('&') > -1) {
-    tokens = process.env.DD_TOKEN.split('&');
-  } else if (process.env.DD_TOKEN.indexOf('@') > -1) {
-    tokens = process.env.DD_TOKEN.split('@');
-  } else if (process.env.DD_TOKEN.indexOf('\n') > -1) {
-    tokens = process.env.DD_TOKEN.split('\n');
-  } else {
-    tokens = [process.env.DD_TOKEN];
-  }
-}
-
 module.exports = tokens;
